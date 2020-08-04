7dcd906bdad637f3c26bccc7d54d2b6b0cb54bc2
izut <52826259+izut@users.noreply.github.com>
2020-08-04 20:04:46 +0800

    Update __init__.py
    完整复制一个sheet到另一个sheet

--- xlutils/__init__.py
+++ xlutils/__init__.py
@@ -3,3 +3,13 @@
 # This Software is released under the MIT License:
 # http://www.opensource.org/licenses/mit-license.html
 # See license.txt for more details.
+
+
+def copy_sheet(from_sheet, to_sheet):
+    """
+    
+    """
+    for k, v in from_sheet.__dict__.items():
+        if k != '_Worksheet__name':
+            setattr(to_sheet, k, v)
+    return to_sheet
